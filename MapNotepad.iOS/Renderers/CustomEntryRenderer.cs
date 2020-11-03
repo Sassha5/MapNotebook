54dfa9dfb22640f2aefc1beaaaa2728149294c8a
Sassha5 <udovikasasha@gmail.com>
2020-11-03 19:16:42 +0200

    - Custom entry renderers - Entry frame style

---
+++ MapNotepad.iOS/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MapNotepad.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace MapNotepad.iOS.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer()
+        {
+            
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.BackgroundColor = null;
+                Control.BorderStyle = UIKit.UITextBorderStyle.None;
+            }
+        }
+    }
+}
